5e64de08e8a0347094d860bb625253067f70c4f7
NathanaelGastelum <nathanaelgastelum@gmail.com>
2025-02-02 22:54:20 -0800

    convert elapsedTime to readable h:m:s format

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Build and run
+gcc -g timeLog.c -lm to build with debugging information and link libm (for fmod function)
--- timeLog.c
+++ timeLog.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 char *action, *activity;
 time_t startTime, currentTime;
@@ -14,7 +17,7 @@ int main(int argc, char *argv[])
     action = argv[1];
     activity = argv[2];
 
-    startTime = time(NULL);
+    startTime = time(NULL); // remove after startTime check is implemented
 
     // Get start time
     if(strcmp(action, "start") == 0)
@@ -28,17 +31,22 @@ int main(int argc, char *argv[])
         // add startTime check once data is logged to file: && startTime != (time_t) NULL
         // enable user to enter elapsed time, to handle cases where start doesn't exist
 
-        currentTime = time(NULL); //TODO: figure out why currentTime getting set to 0
+        currentTime = time(NULL);
         double elapsedTime = difftime(currentTime, startTime);
-        printf("%f spent on %s\n", elapsedTime, activity);
+
+        char startTimeString[8];
+        //strftime(startTimeString, sizeof currentTimeString, "%X", currentTime); // TODO: fix startTime format
+        char *elapsedTimeString;
+        asprintf(&elapsedTimeString, "%.0f:%.0f:%.0f", elapsedTime/3600, (fmod(elapsedTime, 3600.0))/60, fmod(elapsedTime, 60.0)); // convert elapsedTime to readable h:m:s format
+
+        printf("%s spent on %s\n", elapsedTimeString, activity);
 
         // Store elapsed time to a csv logfile
-        FILE *fp = fopen("timeLog.csv", "a"); // TODO: add new csv column headers if file doesn't exist 
-        fprintf(fp, "%d,%s,%f\n", startTime, activity, elapsedTime);
+        FILE *fp = fopen("timeLog.csv", "a"); // TODO: add new csv column headers if file doesn't exist
+        fprintf(fp, "%d,%s,%s\n", startTime, activity, elapsedTimeString);
         fclose(fp);
+        free(elapsedTimeString);
     }
-
-    // convert time to be more readable
 
     // Get today's status
 
